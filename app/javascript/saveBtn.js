9617212ac3fdc51dd59470a76215379545a3c4f4
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-11-25 11:05:17 +0900

    色・画像微調整
    1,編集ボタンのボーダー色
    2,トップ画像の追加
    3,ロゴの色変更

--- app/javascript/saveBtn.js
+++ app/javascript/saveBtn.js
@@ -3,7 +3,7 @@
   const TellSaved = document.getElementById("save-btn")
 
   TellSaved.addEventListener('click', function() {
-    this.setAttribute("style", "background-color: black ;")
+    this.setAttribute("style", "background-color: black; border: 1px solid black;")
     this.setAttribute("data-disable-with", "変更を保存しました");
   })
 }
