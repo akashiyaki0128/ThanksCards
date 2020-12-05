d8e2eeae8235b45c011088a6144ff4878bf7f9d9
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-12-05 16:43:59 +0900

    好きな企業理念モデル修正

--- app/models/philosophy.rb
+++ app/models/philosophy.rb
@@ -8,7 +8,18 @@ class Philosophy < ActiveHash::Base
     { id: 5, name: "社訓"},
     { id: 6, name: "ビション"},
     { id: 7, name: "組織理念"},
-    { id: 8, name: "その他"}
+    { id: 8, name: "自立志向"},
+    { id: 9, name: "共に楽しむ"},
+    { id: 10, name: "ファンを創る"},
+    { id: 11, name: "世界を救う"},
+    { id: 12, name: "夢を描く"},
+    { id: 13, name: "1番を目指す"},
+    { id: 14, name: "ユーザー目線を意識する"},
+    { id: 15, name: "健康を意識する"},
+    { id: 16, name: "ワガママである"},
+    { id: 17, name: "チャレンジを楽しむ"},
+    { id: 18, name: "アップデートし続ける"},
+    { id: 19, name: "その他"}
   ]
   
 end
