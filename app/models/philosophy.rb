807b433ece8683db9556d1e3c3f7b58d03fca585
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-12-05 14:35:57 +0900

    各所編集
    readme、カード表示数、好きな企業理念の編集

--- app/models/philosophy.rb
+++ app/models/philosophy.rb
@@ -4,22 +4,11 @@ class Philosophy < ActiveHash::Base
     { id: 1, name: "---"},
     { id: 2, name: "経営理念" },
     { id: 3, name: "行動指針"},
-    { id: 4, name: "Work Style 8"},
-    { id: 5, name: "Focus Results"},
-    { id: 6, name: "Work Bravely"},
-    { id: 7, name: "Far Beyond"},
-    { id: 8, name: "Talk Straight"},
-    { id: 9, name: "Take Ownership"},
-    { id: 10, name: "Enjoy Together"},
-    { id: 11, name: "会議や打ち合わせは30分以内でしか設定しません！"},
-    { id: 12, name: "定例会議の新設は役員会決議を経ないとできません！"},
-    { id: 13, name: "夜、19時半以降は会社に残れません！"},
-    { id: 14, name: "18時以降は外部内部問わず、原則として打ち合わせはできません！"},
-    { id: 15, name: "社内調整や上司確認で、業務時間内は2時間以上待ちません！"},
-    { id: 16, name: "上司自身はもちろん、メンバーが守れていないと評価されません！"},
-    { id: 17, name: "[使うかもしれない]で、資料やデータを作成/保存しません！"},
-    { id: 18, name: "生産[性]の追求ではないので、感動やチームのための時間は節約しません！"},
-    { id: 19, name: "その他"}
+    { id: 4, name: "提供価値"},
+    { id: 5, name: "社訓"},
+    { id: 6, name: "ビション"},
+    { id: 7, name: "組織理念"},
+    { id: 8, name: "その他"}
   ]
   
 end
